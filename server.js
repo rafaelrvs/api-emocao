eadcc75b198b8bda9f43a55643826b9235dececc
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-19 12:43:57 -0300

    aplcando regras https

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const app = require("./src/app.js")
 const PORT = 8080
 
-app.listen(PORT,()=>{
+
+const https = require('https');
+const fs = require('fs');
+
+const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/clima.amalfis.com.br/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/clima.amalfis.com.br/fullchain.pem')
+}; 
+
+const server = https.createServer(httpsOptions,app);
+
+server.listen(PORT,()=>{
     console.log("Servidor ligado na porta: "+PORT);
     
 })
