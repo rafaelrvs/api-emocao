6c454fed81f94670e407e5cad69e2ad53477adf5
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-21 11:18:09 -0300

    Adicionando cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const usuarios_sentimentos_routes = require("./usuarios_sentimentos_routes.js");
 module.exports = (app) => {
     // Configuração do CORS
     app.use(cors({
-        origin: 'http://localhost:3000', // Porta do frontend
+        origin: 'http://localhost:3000/dashboard', // Porta do frontend
         credentials: true, // Permitir envio de cookies ou autenticação
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
         allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
