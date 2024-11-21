349bdfbd7fa2eac13051d4ce19660b5cf1f74422
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-21 10:58:52 -0300

    Adicionando cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-const cors = require("cors")
-const express  = require("express")
-const emocao_routes =require("./emocao_routes.js")
-const usuarios_sentimentos_routes =require("./usuarios_sentimentos_routes.js")
+const cors = require("cors");
+const express = require("express");
+const emocao_routes = require("./emocao_routes.js");
+const usuarios_sentimentos_routes = require("./usuarios_sentimentos_routes.js");
 
-module.exports = (app) =>{
-    app.use(cors());
+module.exports = (app) => {
+    app.use(cors({
+        origin: 'http://localhost:3000', // ou a porta que seu frontend está rodando
+    }));
     app.use(express.json());
     app.use(emocao_routes);
     app.use(usuarios_sentimentos_routes);
-
-
-}
+};
